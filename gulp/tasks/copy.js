3fbbce01e1b0d09153fe811e9ab83e739857c2f1
vinraelll <53097647+vinraelll@users.noreply.github.com>
2022-09-23 14:24:53 +0300

    only dist initials left

--- gulp/tasks/copy.js
+++
@@ -1,5 +0,0 @@
-// Получаем доступ к исходным файлам, копируем их в папку build
-export const copy = () => {
-  return app.gulp.src(app.path.src.files)
-    .pipe(app.gulp.dest(app.path.build.files))
-}
