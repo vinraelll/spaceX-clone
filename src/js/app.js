74c0485071d7be52ff3da7cf0871bb5755098f0d
vinraelll <53097647+vinraelll@users.noreply.github.com>
2022-09-29 06:01:13 +0300

    added logic of dynamic stats on falcon9 page

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,15 @@
 // DOM elements
-const $hamburger  = document.getElementById('hamburger')
-const $overlay    = document.getElementById('overlay')
-const $mobileMenu = document.getElementById('mobile')
+const $hamburger    = document.getElementById('hamburger')
+const $overlay      = document.getElementById('overlay')
+const $mobileMenu   = document.getElementById('mobile')
+const $statCounters = document.querySelectorAll('.stats__item-count')
+
+let scrollFlag      = false
 
 // Event listeners
 $hamburger.addEventListener('click', toggleMobile) 
 $overlay.addEventListener('click', toggleMobile)
+document.addEventListener('scroll', scrollPage)
 
 // Event handlers 
 function toggleMobile() {
@@ -14,3 +18,50 @@ function toggleMobile() {
   $mobileMenu.classList.toggle('open')
   document.body.classList.toggle('scrollLock')
 }
+
+function scrollPage() {
+  const scrollPos = window.scrollY
+
+  if (scrollPos > 60 && !scrollFlag) {
+    updateFalconStats()
+    scrollFlag = true
+  } else if (scrollPos < 60 && scrollFlag) {
+    resetFalcoStats()
+    scrollFlag = false
+  }
+}
+
+function resetFalcoStats() {
+  $statCounters.forEach(counter => counter.innerText = '0')
+}
+
+function updateFalconStats() {
+  $statCounters.forEach(counter => {
+    counter.innerText = '0'
+
+    const updateCounter = () => {
+      // get count target
+      const target = +counter.getAttribute('data-target')
+
+      // get current target value
+      const c = +counter.innerText
+
+      // create an increment
+      const increment = target / 100
+
+      // if counter is < target, add increment
+      if (c < target) {
+        // round up and set the counter value 
+        counter.innerText = `${Math.ceil(c + increment)}`
+
+        setTimeout(updateCounter, 55)
+      } else {
+        counter.innerText = target
+      }
+    }
+
+    updateCounter() 
+  })
+}
+
+updateFalconStats()
